3c2b257523e1b4e9bdb933893311963b952d0395
El Maxo <max.schulkin@gmail.com>
2016-10-15 21:00:59 -0300

    no hago mas demo

--- src/ojota.cpp
+++ src/ojota.cpp
@@ -3,48 +3,90 @@
 Atleta atletaProdigio(const JJOO &j) const {
     // estado E0;
     // vale REQ: |competenciasConOroEnPodio(j)| > 0;
-    Atleta crack = j.competenciasFinalizadasConOroEnPodio()[0].ranking()[0];
+    int m = 0; Atleta crack = j.competenciasFinalizadasConOroEnPodio()[m].ranking()[0];
     // estado E1;
-    // vale crack == Campeon(competenciasConOroEnPodio(j)[0]);
+    // vale m == 0 && crack == Campeon(competenciasConOroEnPodio(j)[0]);
     int i = 1; //el cero ya arranca en crack, asi que no hace falta mirarlo. Creeme, simplifica las cosas.
     int n = j.competenciasFinalizadasConOroEnPodio().size();
     // estado E2;
-    // vale crack == crack@E1 && i == 1 && n == |competenciasConOroEnPodio(j)|;
-    // Pc: crack == crack@E1 && i == 1 && n == |competenciasConOroEnPodio(j)|;
-    while (i < n){ //Bw: i < n
-        // I: 1 <= i < n &&
+    // vale crack == crack@E1 && i == 1 && m == m@E1 && n == |competenciasConOroEnPodio(j)|;
+    // Pc: crack == crack@E1 && i == 1 && m == m@E1 && n == |competenciasConOroEnPodio(j)| && n > 0(por REQ);
+    while (i < n){ //Bc: i < n
+        // I: 1 =< i =< n && crack == Campeon(competenciasConOroEnPodio(j)[m]) && 0 =< m =< i
         // (forAll k<-[0..i))anioNacimiento(Campeon(competenciasConOroEnPodio(j)[k])) <= anoNacimiento(crack);
         //------------Aclaro lo de arriba: todos los que ya pase, tienen que cumplir la expresión. BORRAR ESTA LINEA----
         // v: |competenciasConOroEnPodio(j)| - i;
         // c: 0;
         // estado C0
-        // vale I && Bw;
-        // Pif: I && Bw;
+        // vale I && Bc;
+        // Pif: I && Bc;
         if (j.competenciasFinalizadasConOroEnPodio()[i].ranking()[0].anioNacimiento() >= crack.anioNacimiento()){
-            //Bif: anioNacimiento(Campeon(competenciasConOroEnPodio(j)[i])) >= anoNacimiento(crack);
+            //Bif: anioNacimiento(Campeon(competenciasConOroEnPodio(j)[i])) >= anoNacimiento(crack@C0);
             // estado Eif0
             // vale Pif && Bif;
-            crack = j.competenciasFinalizadasConOroEnPodio()[i].ranking()[0];
+            m = i; crack = j.competenciasFinalizadasConOroEnPodio()[m].ranking()[0];
             // estado Eif1
-            // vale crack == Campeon(competenciasConOroEnPodio(j)[i]) && Bif;
+            // vale m == i && crack == Campeon(competenciasConOroEnPodio(j)[i]) && Bif;
         }
         // estado C1
-        // Qif: (crack == Campeon(competenciasConOroEnPodio(j)[i]) && Bif) || (crack == crack@E2 && ¬ Bif)
+        // Qif: (crack == Campeon(competenciasConOroEnPodio(j)[m])&& m == i && Bif) || (crack == crack@C0 && ¬ Bif)
         // vale i == i@C0 && Qif;
         i++;
         // estado C2
         // vale i == i@C1 + 1 && Qif
     }
     // estado E3
-    // Qc: i == |competenciasConOroEnPodio(j)| && Qif &&
-    // (forAll i)****anoNacimiento(Campeon(competenciasConOroEnPodio(j)[i])) <= anoNacimiento(crack)
-    // **** obs: hay que decir que no importa el i, esto se cumple.
+    // Qc: Qif &&
+    // (forAll k<-[0..n))anioNacimiento(Campeon(competenciasConOroEnPodio(j)[k])) <= anoNacimiento(crack);
     return crack;
     // estado E4
     // vale i == i@E3 && res == crack@E3;
 }
-// Demostrar el while, y el if.
+// Demostrar el while:
 // Pc -> I:
-// i == 0 -> 0 <= i < |competenciasConOroEnPodio(j)|;
+
+// i == 1 && n > 0 -> 1 =< i =< n;
 // crack == Campeon(competenciasConOroEnPodio(j)[0]) ->
-// anoNacimiento(Campeon(competenciasConOroEnPodio(j)[i])) <= anoNacimiento(crack);
+// (forAll k<-[0..i))anioNacimiento(Campeon(competenciasConOroEnPodio(j)[k])) <= anoNacimiento(crack);
+// crack == Campeon(competenciasConOroEnPodio(j)[0]) && m == 0 -> crack == Campeon(competenciasConOroEnPodio(j)[m]);
+// m == 0 && i == 1 -> 0 =< m =< i;
+
+// -------------------------------------------------------------
+
+//(I && ¬ Bc) -> Qc :
+
+// 1 =< i =< n && ¬ (i < n) -> 1 =< i =< n && i >= n -> i == n;
+// (forAll k<-[0..i))anioNacimiento(Campeon(competenciasConOroEnPodio(j)[k])) <= anoNacimiento(crack) && i == n ->
+// (forAll k<-[0..n))anioNacimiento(Campeon(competenciasConOroEnPodio(j)[k])) <= anoNacimiento(crack);
+// crack == Campeon(competenciasConOroEnPodio(j)[m]) && [ALGUNA COSA] -> Bif || ¬ Bif -> Qif;
+
+//-----------------------------------------------------------------
+
+//(I && B) al inicio -> I al final:
+
+//del if sale que crack == Campeon(competenciasConOroEnPodio(j)[m]), lo unico que cambia es si(m == i || m < i);
+// sigue cumpliendo que (forAll k<-[0..i))anioNacimiento(Campeon(competenciasConOroEnPodio(j)[k])) <= anoNacimiento(crack)
+// -> I;
+
+//--------------------------------------------
+
+// v decreciente:
+
+// v: n - i -> v[i] < v[i+1] -> n - i < n - i + 1;
+
+//--------------------------------------------------------
+
+//(I && v =< c) -> ¬ Bc:
+
+//c: 0 -> v < c == n - i < 0 -> i >= n == ¬ (i < n) -> ¬ Bif;
+//con I sacamos además que n == i;
+
+// el IF:
+
+//RAMA TRUE Pif && Bif :
+//
+
+//------------------------------------------------------
+
+//RAMA FALSE Pif && ¬ Bif :
+//
